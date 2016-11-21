f800d5dea1918d8cdd0ac0911500a01c2515fefb
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2016-11-21 21:33:48 +0900

    修正後のコード

--- src/main/jp/ac/uryukyu/ie/e165725/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165725/Enemy.java
@@ -50,6 +50,9 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
+        if(dead == true){
+            damage = (0 * attack);
+        }
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
     }
--- src/test/jp/ac/uryukyu/ie/e165725/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165725/EnemyTest.java
@@ -10,6 +10,13 @@ import static org.junit.Assert.*;
 public class EnemyTest {
     @Test
     public void attack() throws Exception {
-
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
     }
 }
