931fd9308c925d241b393b2b5843b2dc0a541c4c
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2016-11-21 21:11:15 +0900

    修正前のコード

---
+++ src/test/jp/ac/uryukyu/ie/e165725/EnemyTest.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e165725;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by okuhamamasakiyo on 2016/11/21.
+ */
+public class EnemyTest {
+    @Test
+    public void attack() throws Exception {
+
+    }
+}
